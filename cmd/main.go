31e578a6e53293ef8adccd8d61768b9f2da6ab98
Vasiliy Andrianov <veandrianov@avito.ru>
2021-04-23 17:12:15 +0300

    stop bot

--- cmd/main.go
+++ cmd/main.go
@@ -8,8 +8,6 @@ import (
 
 	"github.com/Andrianov/psychoHelpBOT/internal"
 	"github.com/Andrianov/psychoHelpBOT/internal/config"
-	"github.com/Andrianov/psychoHelpBOT/internal/handlers/cancel"
-	_go "github.com/Andrianov/psychoHelpBOT/internal/handlers/go"
 	"github.com/Andrianov/psychoHelpBOT/internal/handlers/message"
 	"github.com/Andrianov/psychoHelpBOT/internal/handlers/start"
 	"github.com/Andrianov/psychoHelpBOT/internal/router"
@@ -35,8 +33,8 @@ func main() {
 
 	router := router.New()
 	router.RegisterCommandHandler("start", start.New(cfg, bot, storage))
-	router.RegisterCommandHandler("go", _go.New(cfg, bot, storage))
-	router.RegisterCommandHandler("cancel", cancel.New(cfg, bot, storage))
+	//router.RegisterCommandHandler("go", _go.New(cfg, bot, storage))
+	//router.RegisterCommandHandler("cancel", cancel.New(cfg, bot, storage))
 	router.RegisterMessageHandler(message.New(cfg, bot, storage))
 
 	app := internal.New(cfg, bot, router)
